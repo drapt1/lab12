111a57e7efebdb4e74ed3a3859a246dbabdac019
Костаревы <@->
2022-08-09 16:34:52 +0500

    Добавьте файлы проекта.

---
+++ lab1.1/lab1.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+int main()
+{
+    //тип данных и переменных
+    double b, otv, up, low, a, q, w;
+    // известные переменные
+    a = 1000;
+    b = 0.0001;
+    q = 2;
+    w = 3;
+    // вычисление верхней части, pow - возведение в степень
+    up = pow((a - b), w) - pow(a, w);
+    // вычисление нижней части
+    low = pow(b, w) - 3 * a * pow(b, q) - 3 * pow(a, q) * b;
+    //вычисление ответа
+    otv = up / low;
+    // вывод ответа
+    cout << otv << endl;
+    return 0;
+}

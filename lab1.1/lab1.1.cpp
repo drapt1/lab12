cdc65e9ad6ca7ed07a93b998b1d0226845a76cbd
drapt1 <110332491+drapt1@users.noreply.github.com>
2022-08-20 16:44:11 +0500

    Update lab1.1.cpp

--- lab1.1/lab1.1.cpp
+++ lab1.1/lab1.1.cpp
@@ -1,24 +1,18 @@
 ﻿#include <iostream>
 #include <cmath>
 
-
 using namespace std;
+
 int main()
 {
-    //тип данных и переменных
-    double b, otv, up, low, a, q, w;
-    // известные переменные
-    a = 1000;
+    double b, otv, up, low, a, q, w;    //тип данных и переменных
+    a = 1000;                           // известные переменные
     b = 0.0001;
     q = 2;
     w = 3;
-    // вычисление верхней части, pow - возведение в степень
-    up = pow((a - b), w) - pow(a, w);
-    // вычисление нижней части
-    low = pow(b, w) - 3 * a * pow(b, q) - 3 * pow(a, q) * b;
-    //вычисление ответа
-    otv = up / low;
-    // вывод ответа
-    cout << otv << endl;
+    up = pow((a - b), w) - pow(a, w);   // вычисление верхней части, pow - возведение в степень
+    low = pow(b, w) - 3 * a * pow(b, q) - 3 * pow(a, q) * b;// вычисление нижней части
+    otv = up / low;                     //вычисление ответа
+    cout << otv << endl;                // вывод ответа
     return 0;
 }
